38a810b5d2844f5bd3f9f285b3c8d2dae08e8139
alessio1887 <uruz90@hotmail.it>
2024-09-17 17:44:03 +0200

    Copyright corrected

--- plugins.js
+++ plugins.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2024, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import DateRangePickerPlugin from '@js/plugins/DateRangePicker';
 import DecadeRangePickerPlugin from '@js/plugins/DecadeRangePicker';
 import productPlugins from '@mapstore/product/plugins.js';
--- utils/CheckLayerVariabiliMeteoUtils.jsx
+++ utils/CheckLayerVariabiliMeteoUtils.jsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2024, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 const PIOGGIA = ["Pioggia_Anomalia_perc", "Pioggia_Anomalia_mm", "Pioggia_Cumulata"];
 const TEMPERATURA = ["Temperatura_Media", "Temperatura_Media_Anomalia",
     "Temperatura_Minima", "Temperatura_Minima_Anomalia", "Temperatura_Massima", "Temperatura_Massima_Anomalia"];
