7266ff3bc6b554076ad88c054d7992703090b26a
alessio1887 <uruz90@hotmail.it>
2024-12-04 15:39:55 +0100

    Restored previous version of plugins.js; removed constants from localConfig.json.

--- plugins.js
+++ plugins.js
@@ -5,9 +5,11 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import FreeRangePickerPlugin from '@js/plugins/FreeRangePicker';
+import FixedRangePickerPlugin from '@js/plugins/FixedRangePicker';
+import InfoChartPlugin from './plugins/InfoChart';
+import DateRangeLabelPlugin from './plugins/DateRangeLabel';
 import productPlugins from '@mapstore/product/plugins.js';
-import {toModulePlugin} from "@mapstore/utils/ModulePluginsUtils";
-
 
 export default {
     requires: {
@@ -15,9 +17,9 @@ export default {
     },
     plugins: {
         ...productPlugins.plugins,
-        FreeRangePickerPlugin: toModulePlugin('FreeRangePicker', () => import( '@js/plugins/FreeRangePicker')),
-        FixedRangePickerPlugin: toModulePlugin('FixedRangePicker', () => import( '@js/plugins/FixedRangePicker')),
-        DateRangeLabelPlugin: toModulePlugin('DateRangeLabel', () => import( '@js/plugins/DateRangeLabel')),
-        InfoChartPlugin: toModulePlugin('InfoChartPlugin', () => import( '@js/plugins/InfoChart'))
+        FreeRangePickerPlugin,
+        FixedRangePickerPlugin,
+        DateRangeLabelPlugin,
+        InfoChartPlugin
     }
 };
