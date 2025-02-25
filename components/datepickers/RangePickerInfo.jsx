a04fa9585670358885a23a18bb539ee72520c164
alessio1887 <uruz90@hotmail.it>
2025-02-25 09:31:20 +0100

    Added loading spinner with text in RangePickerInfo component

--- components/datepickers/RangePickerInfo.jsx
+++ components/datepickers/RangePickerInfo.jsx
@@ -1,42 +1,57 @@
-/*
- * Copyright 2024, Consorzio LaMMA.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import React from 'react';
 import { Label } from 'react-bootstrap';
 import Message from '../../../MapStore2/web/client/components/I18N/Message';
 import { DATE_FORMAT } from '../../utils/ManageDateUtils';
 import moment from 'moment';
 import momentLocaliser from 'react-widgets/lib/localizers/moment';
+import Spinner from 'react-spinkit'; // Assumendo che tu stia usando react-spinkit per lo spinner
 momentLocaliser(moment);
 
 import './rangepickerinfo.css';
 
-const RangePickerInfo = ({ fromData,
+const RangePickerInfo = ({
+    fromData,
     toData,
     labelTitleId,
     format,
     isInteractionDisabled
-}) => (
-    <div className="labels-container">
-        <Label className="labels-rangepicker">
-            <Message msgId={labelTitleId} />
-        </Label>
-        <strong>
-            <div style={{
-                padding: "6px",
-                textAlign: 'center',
-                backgroundColor: isInteractionDisabled ? 'lightgray' : 'white'
-            }}>
-            Dal: <span id="from-data-statistics">{moment(fromData).format(format)}</span>
-                {format !== DATE_FORMAT && <br />}
-            - al: <span id="to-data-statistics">{moment(toData).format(format)}</span>
+}) => {
+    // Funzione per visualizzare lo spinner
+    const renderLoading = () => (
+        <div className="loading-overlay" aria-live="assertive">
+            <div className="loading-spinner">
+                <Message msgId="gcapp.loading" />
+                <Spinner spinnerName="circle" noFadeIn overrideSpinnerClassName="spinner" />
             </div>
-        </strong>
-    </div>
-);
+        </div>
+    );
+
+    const renderPickerInfo = () => (
+        <>
+         Dal: <span id="from-data-statistics">{moment(fromData).format(format)}</span>
+            {format !== DATE_FORMAT && <br />}
+        - al: <span id="to-data-statistics">{moment(toData).format(format)}</span>
+        </>
+    );
+
+    return (
+        <div className="labels-container">
+            <Label className="labels-rangepicker">
+                <Message msgId={labelTitleId} />
+            </Label>
+            <strong>
+                <div
+                    style={{
+                        padding: "6px",
+                        textAlign: 'center',
+                        backgroundColor: isInteractionDisabled ? 'lightgray' : 'white'
+                    }}
+                >
+                    { true ? ( renderLoading() ) : ( renderPickerInfo() )}
+                </div>
+            </strong>
+        </div>
+    );
+};
 
 export default RangePickerInfo;
