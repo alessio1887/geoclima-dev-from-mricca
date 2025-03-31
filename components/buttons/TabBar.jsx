5d9978d18b2e3e6ef825344fe5fcbd745163bc65
alessio1887 <uruz90@hotmail.it>
2025-03-31 14:18:03 +0200

    Corrected TabBar styles in ExportImage plugin

--- components/buttons/TabBar.jsx
+++ components/buttons/TabBar.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 const TabBar = ({ tabList = [], onChangeTab, activeTab, classAttribute }) => {
     const selectedTab = activeTab || (tabList.length > 0 ? tabList[0] : null);
+    // // Combines the provided class (classAttribute) with the suffix '-tabbar' to create a unique CSS class for the TabBar container.
     const containerClass = classAttribute + "-tabbar";
-
     return (
         <div className={containerClass}>
             {tabList.map(tab => {
                 const itemClass = containerClass + "-item";
-
                 return (
                     <div
                         key={tab.id}
